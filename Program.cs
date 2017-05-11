ad1d47e1ee9116199ad3eb475acdf2de1eb0b3e0
MrForester82 <potate82@ya.ru>
2017-05-11 15:04:44 +0300

    3 - Задание 1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_sharp_lab_5
 {
@@ -18,6 +14,122 @@ namespace C_sharp_lab_5
             Console.WriteLine(str1);
             Console.WriteLine(str2);
             Console.WriteLine(str3);
+            Console.WriteLine();
+
+            assignment(str1, str2, str3);
+
+            equal(str1, str2, str3);
+
+            concat(str1, str2, str3);
+
+            index(str1, str2, str3);
+
+            comments("Жить - хорошо (а хорошо жить - ещё лучше) ...");
+
+            delStudent("Каждый student в душе ленивец.", "student");
         }
+
+        // Задание 1 --------------------------------------------------------------------------
+        public static void assignment(string str1, string str2, string str3)
+        {
+            str1 = "test1";
+            str2 = "test2";
+            str3 = "test3";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.WriteLine();
+        }
+
+        public static void equal(string str1, string str2, string str3)
+        {
+            if(str1 == "Строка1")
+                Console.WriteLine("Строка str1 имеет значение {0}", str1);
+            else
+                Console.WriteLine("Строка str1 не имеет значение {0}", str1);
+
+            if (str2 == "sssss")
+                Console.WriteLine("Строка str2 имеет значение {0}", str2);
+            else
+                Console.WriteLine("Строка str2 не имеет значение {0}", str2);
+
+            if (str3 == "test")
+                Console.WriteLine("Строка str3 имеет значение {0}", str3);
+            else
+                Console.WriteLine("Строка str3 не имеет значение {0}", str3);
+            
+            Console.WriteLine();
+        }
+
+        public static void concat(string str1, string str2, string str3)
+        {
+            Console.WriteLine(str1 + str2);
+            Console.WriteLine(str2 + str3);
+            Console.WriteLine(str1 + str3);
+            Console.WriteLine();
+        }
+
+        public static void index(string str1, string str2, string str3)
+        {
+            Console.WriteLine(str1[3]);
+            Console.WriteLine(str2[0]);
+            Console.WriteLine(str3[2]);
+            Console.WriteLine();
+        }
+
+        public static void comments(string str1)
+        {
+            int start = str1.IndexOf('(');
+            int finish = str1.IndexOf(')');
+
+            if (start != 0 && finish != 0 && start < finish)
+            {
+                Console.WriteLine("Исходная строка: {0}", str1);
+
+                str1 = str1.Substring(start+1);
+                finish = str1.IndexOf(')');
+                str1 = str1.Remove(finish);
+
+                Console.WriteLine("Её комментарий: {0}", str1);
+            }
+            else
+            {
+                Console.WriteLine("В данной строке нет комментариев");
+            }
+            
+            Console.WriteLine();
+        }
+
+        public static void delStudent(string str1, string todel)
+        {
+            int start = str1.IndexOf(todel);
+            int finish = str1.IndexOf(todel) + todel.Length;
+            string str2, str3;
+
+            Console.WriteLine(finish);
+
+            if (start != 0)
+            {
+                Console.WriteLine("Исходная строка: {0}", str1);
+
+                str2 = str1.Remove(start);
+                str3 = str1.Substring(finish);
+                
+                Console.WriteLine(str3);
+
+                Console.WriteLine("Новая строка: {0}", str2 + str3);
+            }
+            else
+            {
+                Console.WriteLine(str1);
+            }
+
+            Console.WriteLine();
+        }
+        // Задание 1 --------------------------------------------------------------------------
+
+        // Задание 2 --------------------------------------------------------------------------
+
     }
 }

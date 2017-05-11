c4f27f73ee3a36f14cf841ae8cfc627362971e52
MrForester82 <potate82@ya.ru>
2017-05-11 11:36:25 +0300

    1 - Создание проекта
    Новый пустой проект

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

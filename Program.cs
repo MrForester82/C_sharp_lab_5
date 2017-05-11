4274c8d4bcc83becb489907d56a3ee6fc7301b81
MrForester82 <potate82@ya.ru>
2017-05-11 13:47:38 +0300

    2 - Сборка проекта
    Объявление и инициализация переменных
    Сборка проекта

--- Program.cs
+++ Program.cs
@@ -10,7 +10,14 @@ namespace C_sharp_lab_5
     {
         static void Main(string[] args)
         {
+            string str1 = "Строка1";
+            string str2 = new string('s', 5);
+            char[] charray = { 't', 'e', 's', 't' };
+            string str3 = new string(charray);
 
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
         }
     }
 }
